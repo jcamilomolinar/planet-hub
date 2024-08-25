16bb033fc2b8c9cf449f5e9b1d94c6cf8b11cbd8
jcamilomolinar <jmolinaro@unal.edu.co>
2024-08-25 00:50:26 -0500

    feat(flight form): fix field standarization

--- planet-hub/components/FlightSearchForm.tsx
+++ planet-hub/components/FlightSearchForm.tsx
@@ -1,28 +1,19 @@
 "use client"
 
-import * as React from "react"
-import Link from 'next/link'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-import { DatePickerWithRange } from "@/components/DatePickerWithRange";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Check, ChevronsUpDown } from "lucide-react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
 import {
   Form,
   FormControl,
@@ -32,8 +23,81 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
-import { Input } from "@/components/ui/input";
-import { FlightSearchFormField } from "@/components/FlightSearchFormField";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+function FlightSearchFormField({ elements, form, title, name, description }:
+    { elements: any, form: any, name: string, title: string, description: string }) {
+  
+    return (
+        <FormField
+        control={form.control}
+        name={name}
+        render={({ field }:{field: any}) => (
+          <FormItem className="flex flex-col">
+            <FormLabel>{title}</FormLabel>
+            <Popover>
+              <PopoverTrigger asChild>
+                <FormControl>
+                  <Button
+                    variant="outline"
+                    role="combobox"
+                    className={cn(
+                      "w-[200px] justify-between",
+                      !field.value && "text-muted-foreground"
+                    )}
+                  >
+                    {field.value
+                      ? elements.find(
+                          (element: any) => element.value === field.value
+                        )?.label
+                      : "Select an option"}
+                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                  </Button>
+                </FormControl>
+              </PopoverTrigger>
+              <PopoverContent className="w-[200px] p-0">
+                <Command>
+                  <CommandInput placeholder="Search option..." />
+                  <CommandList>
+                    <CommandEmpty>Not found</CommandEmpty>
+                    <CommandGroup>
+                      {elements.map((element: any) => (
+                        <CommandItem
+                          value={element.label}
+                          key={element.value}
+                          onSelect={() => {
+                            form.setValue(name, element.value)
+                          }}
+                        >
+                          <Check
+                            className={cn(
+                              "mr-2 h-4 w-4",
+                              element.value === field.value
+                                ? "opacity-100"
+                                : "opacity-0"
+                            )}
+                          />
+                          {element.label}
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
+              </PopoverContent>
+            </Popover>
+            <FormDescription>
+              {description}
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    );
+}
 
 const languages = [
     { label: "English", value: "en" },
@@ -45,49 +109,29 @@ const languages = [
     { label: "Japanese", value: "ja" },
     { label: "Korean", value: "ko" },
     { label: "Chinese", value: "zh" },
-  ]
-
-const formSchema = z.object({
+  ] as const
+  
+  const FormSchema = z.object({
     language: z.string({
-        required_error: "Please select an option.",
-    })
-})
-
-function ProfileForm() {
-    const form = useForm<z.infer<typeof formSchema>>({
-      resolver: zodResolver(formSchema),
-    })
-
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values)
-    }
-    
-    return (
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FlightSearchFormField
-                    elements={languages}
-                    form={form}
-                    title="Title"
-                    name="planet"
-                    description="Description" 
-            />
-            <Button type="submit" variant="outline">Submit</Button>
-          </form>
-        </Form>
-      )
-}
+      required_error: "Please select a language.",
+    }),
+  })
 
 export function FlightSearchForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
+
   return (
-    <Card className="w-auto">
-      <CardHeader>
-        <CardTitle>Search a destination!</CardTitle>
-        <CardDescription>Find flights to the planet of your choice, search for new destinations according to their characteristics!</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <ProfileForm />
-      </CardContent>
-    </Card>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FlightSearchFormField elements={languages} form={form} title="Title" name="language" description="exampleeeeeeeee"/>
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
