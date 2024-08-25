0ddffaa3b854785fc2985f5b3e56f44962565a5d
jcamilomolinar <jmolinaro@unal.edu.co>
2024-08-25 13:15:47 -0500

    fix(flight form): add date picker

--- planet-hub/components/FlightSearchForm.tsx
+++ planet-hub/components/FlightSearchForm.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Check, ChevronsUpDown } from "lucide-react"
 import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { date, z } from "zod"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -32,11 +32,70 @@ import {
 import {
   Card,
   CardContent,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { format } from "date-fns"
+import { CalendarIcon } from "lucide-react"
+import { Calendar } from "@/components/ui/calendar"
+
+function FlightSearchFormFieldDate({ form, title, name, description }:
+  { form: any, name: string, title: string, description: string }){
   
+  return(
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{title}</FormLabel>
+          <FormControl>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant={"outline"}
+                  className={cn(
+                    "w-full justify-start text-left font-normal",
+                    !field.value && "text-muted-foreground",
+                  )}
+                >
+                  <CalendarIcon className="mr-2 h-4 w-4" />
+                  {field.value?.from ? (
+                    field.value.to ? (
+                      <>
+                        {format(field.value.from, "LLL dd, y")} -{" "}
+                        {format(field.value.to, "LLL dd, y")}
+                      </>
+                    ) : (
+                      format(field.value.from, "LLL dd, y")
+                    )
+                  ) : (
+                    <span>Pick a date</span>
+                  )}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                  initialFocus
+                  mode="range"
+                  defaultMonth={field.value?.from}
+                  selected={field.value}
+                  onSelect={field.onChange}
+                  numberOfMonths={2}
+                />
+              </PopoverContent>
+            </Popover>
+          </FormControl>
+          <FormDescription>
+            {description}
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
+
 function FlightSearchFormFieldInput({ form, title, name, description }:
   { form: any, name: string, title: string, description: string }) {
   
@@ -142,14 +201,22 @@ const languages = [
     { label: "Chinese", value: "zh" },
   ] as const
   
+  const DATE_REQUIRED_ERROR = "Date is required.";
+
   const FormSchema = z.object({
     tour: z.string(),
     weather: z.string(),
     activityType: z.string(),
     puntuation: z.string(),
     price: z.coerce.number().nonnegative(),
-    timeTravel: z.coerce.number().nonnegative()
-  })
+    timeTravel: z.coerce.number().nonnegative(),
+    date: z.object({
+            from: z.date().optional(),
+            to: z.date().optional(),
+          }, {required_error: DATE_REQUIRED_ERROR}).refine((date) => {
+          return !!date.from;
+          }, DATE_REQUIRED_ERROR),
+    })
 
 export function FlightSearchForm() {
   const form = useForm<z.infer<typeof FormSchema>>({
@@ -176,14 +243,19 @@ export function FlightSearchForm() {
             <CardTitle className="text-textTitle">Search Flights</CardTitle>
           </CardHeader>
           <CardContent className="text-textAll">
-            <div className="grid grid-cols-3 grid-rows-2 gap-6">
-              <FlightSearchFormFieldCombobox elements={languages} form={form} title="Tours" name="tour" description="Select the tour of your preference." />
-              <FlightSearchFormFieldCombobox elements={languages} form={form} title="Weather" name="weather" description="Select the weather of your preference." />
-              <FlightSearchFormFieldCombobox elements={languages} form={form} title="Type of activity" name="activityType" description="Select the type of activity of your preference." />
-              <FlightSearchFormFieldCombobox elements={languages} form={form} title="Puntuation" name="puntuation" description="Select the minimum puntuation you want for your destination" />
-              <FlightSearchFormFieldInput form={form} title="Price" name="price" description="Enter your maximum budget (0 if you don't have a maximum)." />
-              <FlightSearchFormFieldInput form={form} title="Time travel" name="timeTravel" description="Enter your desired maximum travel time (0 if you don't have a maximum)." />
-            </div>
+            <div>
+              <div className="grid grid-cols-4 grid-rows-1 gap-6">
+                <FlightSearchFormFieldCombobox elements={languages} form={form} title="Tours" name="tour" description="Select the tour of your preference." />
+                <FlightSearchFormFieldCombobox elements={languages} form={form} title="Weather" name="weather" description="Select the weather of your preference." />
+                <FlightSearchFormFieldCombobox elements={languages} form={form} title="Type of activity" name="activityType" description="Select the type of activity of your preference." />
+                <FlightSearchFormFieldCombobox elements={languages} form={form} title="Puntuation" name="puntuation" description="Select the minimum puntuation you want for your destination" />
+              </div>
+              <div className="grid grid-cols-3 grid-rows-1 gap-6">
+                <FlightSearchFormFieldDate form={form} title="Travel date" name="date" description="Enter when you will travel." />
+                <FlightSearchFormFieldInput form={form} title="Price" name="price" description="Enter your maximum budget (0 if you don't have a maximum)." />
+                <FlightSearchFormFieldInput form={form} title="Time travel" name="timeTravel" description="Enter your desired maximum travel time (0 if you don't have a maximum)." />
+              </div>
+           </div>
             <Button className="my-5" type="submit" variant="outline">Search!</Button>
           </CardContent>
         </Card>
